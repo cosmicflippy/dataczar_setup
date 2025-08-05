c73959e018345c8bdd94d0a6d5330cc21d68cdef
kaihickey <kai.hickey@churchillre.com>
2025-08-05 12:36:48 -0400

    feature: changed to firefox and fixed screen rotation

--- initial_setup.sh
+++ initial_setup.sh
@@ -5,6 +5,7 @@ sudo apt install python3 python3-pip python3-venv -y
 
 mkdir dataczar
 cd dataczar
+
 python3 -m venv venv
 source venv/bin/activate
 
@@ -14,8 +15,14 @@ pip install python-dotenv
 playwright install
 playwright install-deps
 
+echo "Grabbing setup files..."
+
+sudo curl -sSL https://raw.githubusercontent.com/cosmicflippy/dataczar_setup/refs/head/main/gnome-randr.py -o gnome-randr.py
 sudo curl -sSL https://raw.githubusercontent.com/cosmicflippy/dataczar_setup/refs/heads/main/main.py -o main.py
 sudo curl -sSL https://raw.githubusercontent.com/cosmicflippy/dataczar_setup/refs/heads/main/dataczar.service -o dataczar.service
+sudo curl -sSL https://raw.githubusercontent.com/cosmicflippy/dataczar_setup/refs/heads/main/rotate.service -o rotate.service
+
+echo "Successfully grabbed files."
 
 echo "Setting up the .env file..."
 if [ ! -f .env ]; then
@@ -25,10 +32,12 @@ else
     echo ".env file already exists. Please edit it as needed."
 fi
 
-echo "Enabling the dataczar service to run on boot..."
+echo "Enabling the services to run on boot..."
 
 mv dataczar.service /etc/systemd/system/dataczar.service
+mv rotate.service /etc/systemd/system/rotate.service
 sudo systemctl daemon-reload
 sudo systemctl enable dataczar.service
+sudo systemctl enable rotate.service
 
 echo "Please set your environment variables in the .env file, then reboot your device."
