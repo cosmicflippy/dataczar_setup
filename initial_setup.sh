0c3988f1b929673c745083fbedf66c8449d86838
kaihickey <kai.hickey@churchillre.com>
2025-08-20 15:06:58 -0400

    feature: added kiosk.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,5 +1,7 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
+curl -sSL https://raw.githubusercontent.com/cosmicflippy/dataczar_setup/refs/heads/main/kiosk.sh -o kiosk.sh
+
 sudo apt install cmake scdoc pkg-config checkinstall git
 cd Downloads
 
@@ -20,3 +22,4 @@ echo "5. y"
 sudo echo "YDOTOOL_SOCKET=/tmp/.ydotool_socket" >> ~/.profile
 
 echo "Once done with that, log out then log back in, or reboot."
+echo "Also, change screen orientation through the GUI"
---
+++ kiosk.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+WAYLAND_DISPLAY=wayland-1 MOZ_ENABLE_WAYLAND=1 firefox-esr --kiosk "https://cnc.churchill-realestate.com" &
+
+set -a
+source .env
+set +a
+
+sleep 5
+ydotool key 56:1 15:1 15:0 56:0 #alt + tab
+sleep 1
+ydotool key 15:1 15:0 #tab key
+ydotool key 15:1 15:0
+sleep 2
+ydotool type $USERNAME
+sleep 2
+ydotool key 15:1 15:0 #tab key
+ydotool type $PASSWORD
+sleep 2
+ydotool key 28:1 28:0 #enter key
+sleep 10
+ydotool key 15:1 15:0 #tab key 6 times
+ydotool key 15:1 15:0
+ydotool key 15:1 15:0
+ydotool key 15:1 15:0
+ydotool key 15:1 15:0
+ydotool key 15:1 15:0
+sleep 1
+ydotool key 15:1 15:0
+ydotool key 28:1 28:0 #enter key
+sleep 3
+ydotool key 29:1 12:1 29:0 12:0 # ctrl + - for zooming out
+ydotool key 29:1 12:1 29:0 12:0
