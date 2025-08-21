4c9f901f1c9e8dae07ee1ae183f7f1183821b8b6
kaihickey <kai.hickey@churchillre.com>
2025-08-21 11:51:06 -0400

    fixed sleep

--- initial_setup.sh
+++ initial_setup.sh
@@ -4,9 +4,10 @@ sudo apt-get update && sudo apt-get upgrade -y
 curl -sSL https://raw.githubusercontent.com/cosmicflippy/dataczar_setup/refs/heads/main/kiosk.sh -o kiosk.sh
 curl -sSL https://raw.githubusercontent.com/cosmicflippy/dataczar_setup/refs/heads/main/.env -o .env
 
+
 echo "Finished upgrading. Downloading dependencies..."
 
-sleep(3)
+sleep 5
 
 sudo apt install cmake scdoc pkg-config checkinstall git software-properties-common firefox-esr -y
 echo "Finished installing dependencies. Setting up ydotool..."
@@ -18,10 +19,6 @@ mkdir build && cd build
 cmake ..
 make -j `nproc`
 
-echo "Run the commands below with the following settings"
-echo "Change to Wayfire in raspi-config > System options > Advanced"
-sudo "Go into keyboard settings (top left) Change keyboard locale to UK, then back to US. Doesn't seem to want to work on setup."
-echo ""
 echo "ydotool is ready to be configured/setup. Follow the instructions below."
 echo "Command 1: cd Downloads/ydotool/build"
 echo "Command 2: sudo checkinstall --fstrans=no"
@@ -32,7 +29,11 @@ echo "3. press 2 then change the name to ydotool-custom"
 echo "4. n"
 echo "5. y"
 
-sudo echo "export YDOTOOL_SOCKET=/tmp/.ydotool_socket" >> ~/.profile
+echo "Run the commands below with the following settings"
+echo "Change to Wayfire in raspi-config > System options > Advanced"
+sudo "Go into keyboard settings (top left) Change keyboard locale to UK, then back to US. Doesn't seem to want to work on setup."
+echo "Command 3: cd ~/Downloads/ydotool/build && sudo mv ydotoold.serice /etc/systemd/system/"
+echo "Command 4: sudo systemctl enable ydotoold && sudo systemctl start ydotoold"
+echo "Command 5: sudo echo 'export YDOTOOL_SOCKET=/tmp/.ydotool_socket' >> ~/.profile"
 
-echo "Change the screen orientation through the GUI"
-echo "Once done with all of that, reboot."
+echo "Once done with all of that, reboot and run the script."
