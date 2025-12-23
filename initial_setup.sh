2d80fb27ab072ae9aa1ae7ebc76d3b9e2796a9dd
kaihickey <kai.hickey@churchillre.com>
2025-12-23 15:17:25 -0500

    Updating for less user-input

--- initial_setup.sh
+++ initial_setup.sh
@@ -7,7 +7,7 @@ curl -sSL https://raw.githubusercontent.com/cosmicflippy/dataczar_setup/refs/hea
 
 echo "Finished upgrading. Downloading dependencies..."
 
-sleep 5
+sleep 2
 
 sudo apt install cmake scdoc pkg-config checkinstall git software-properties-common firefox-esr -y
 echo "Finished installing dependencies. Setting up ydotool..."
@@ -19,21 +19,17 @@ mkdir build && cd build
 cmake ..
 make -j `nproc`
 
-echo "ydotool is ready to be configured/setup. Follow the instructions below."
-echo "Command 1: cd Downloads/ydotool/build"
-echo "Command 2: sudo checkinstall --fstrans=no"
-echo "Follow these instructions for each step."
-echo "1. y"
-echo "2. Enter a description. Something like 'automated keyboard'"
-echo "3. press 2 then change the name to ydotool-custom"
-echo "4. n"
-echo "5. y"
+echo "Setting up ydotool..."
+sleep 2
+cd Downloads/ydotool/build
+sudo checkinstall --default \ --pkgname="ydotool-custom" --fstrans=no
 
-echo "Run the commands below with the following settings"
-echo "Change to Wayfire in raspi-config > System options > Advanced"
-sudo "Go into keyboard settings (top left) Change keyboard locale to UK, then back to US. Doesn't seem to want to work on setup."
-echo "Command 3: cd ~/Downloads/ydotool/build && sudo mv ydotoold.serice /etc/systemd/system/"
-echo "Command 4: sudo systemctl enable ydotoold && sudo systemctl start ydotoold"
-echo "Command 5: sudo echo 'export YDOTOOL_SOCKET=/tmp/.ydotool_socket' >> ~/.profile"
+echo "Running the commands..."
+cd ~/Downloads/ydotool/build && sudo mv ydotoold.service /etc/systemd/system/
+sudo systemctl enable ydotoold && sudo systemctl start ydotoold
+sudo echo 'export YDOTOOL_SOCKET=/tmp/.ydotool_socket' >> ~/.profile
+echo "Setting up permissions..."
+sudo chown -R $USER:$USER /tmp/.ydotool_socket
+sudo chmod -R 660 /tmp/.ydotool_socket
 
-echo "Once done with all of that, reboot and run the script."
+echo "Reboot and run the kiosk.sh script"
